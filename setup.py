2e0c35862d65ac744585eea6011c557cdad74b9a
Josh Gibson <josh-gibson@outlook.com>
2021-02-21 14:00:34 +0000

    Correct path to README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import pathlib
 from setuptools import setup, find_packages
 
 
-with open(pathlib.Path(__file__) / 'README.md', 'r', encoding='utf-8') as fh:
+with open(pathlib.Path(__file__).parent / 'README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(name='pytadolog',
