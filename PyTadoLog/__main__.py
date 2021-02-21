a05e04e23a3d612a1c0d489f056d13f9981166d7
Josh Gibson <josh-gibson@outlook.com>
2021-02-21 13:30:53 +0000

    Add main script with argument parser

--- PyTadoLog/__main__.py
+++ PyTadoLog/__main__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''Main module for running TadoLogger from commandline.'''
+import argparse
 import datetime as dt
 
 from . import TadoLogger
@@ -9,8 +10,20 @@ from . import TadoLogger
 
 def main():
     '''Launch the TadoLogger.'''
-    #TODO: Add argument parser to let user run logging or convert csv to excel
-    with TadoLogger() as tl:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--outdir', help='Path to output directory')
+    parser.add_argument('--update-period', type=int,
+                        help='Time in seconds between updates',
+                        )
+    parser.add_argument('--last-day', help='Last day of week as 3 letter string')
+    parser.add_argument('-m', '--multiprocessing', action='store_true',
+                        help='Use separate processed for web query and csv saving')
+    args = parser.parse_args()
+    with TadoLogger(args.outdir,
+                    args.update_period,
+                    args.last_day,
+                    args.multiprocessing,
+                    ) as tl:
         tl.start()
     print(f'Logging ended at {dt.datetime.now()}')
 
