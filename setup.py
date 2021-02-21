d3d5e44ca21d9313201cd677114834239ed48645
Josh Gibson <josh-gibson@outlook.com>
2021-02-21 13:41:35 +0000

    Rename package to conform with PEP8

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pathlib
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open(pathlib.Path(__file__) / 'README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
-setup(name='PyTadoLog',
+setup(name='pytadolog',
       version='0.1.0',
       description='CSV logger for PyTado from Josh Gibson',
       long_description=long_description,
-      keywords='tado',
+      keywords=['tado', 'python', 'logging'],
       author='Josh Gibson',
       author_email='josh-gibson@outlook.com',
       url='https://github.com/lpforthewin/TadoLogger',
@@ -24,8 +25,8 @@ setup(name='PyTadoLog',
         # Get PyTado from GitHub because PyPi version doesn't work with Python3
         'python-tado @ git+https://git@github.com/chrism0dwk/PyTado.git'
       ],
-      license="MIT",
-      platforms=["any"],
+      license='MIT',
+      platforms=['any'],
       packages=find_packages(),
       classifiers=[
         'Development Status :: 3 - Alpha',
